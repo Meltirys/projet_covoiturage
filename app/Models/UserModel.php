05a056caa915faf1e0fd5bc5b48e36b79d74c300
Zadig <odium@riseup.net>
2026-05-06 16:21:31 +0200

    added validation rules for user data

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use App\Validation\CustomRules;
 
 class UserModel extends Model
 {
@@ -29,14 +30,30 @@ class UserModel extends Model
 
     // Validation
     protected $validationRules      = [
-        'email' => 'required|valid_email',
+        'first_name' => 'required|min_length[2]|max_length(100)',
+        'last_name' => 'required|min_length[2]|max_length(100)',
+        'email' => 'required|valid_email|is_unique[users.email]',
         'password' => 'required|min_length[8]|max_length[255]|regex_match[/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]).+$/]',
+        'mobile' => 'permit_empty|regex_match[/^(?:(?:\+33|0)[67])(?:[\s.-]?\d{2}){4}$/]',
+        'birth_date' => 'required|valid_date[Y-m-d]|adultCheck',
+        'gender' => 'required|in_list[homme,femme,autre]',
     ];
 
     protected $validationMessages   = [
+        'first_name' => [
+            'required' => 'Prénom requis',
+            'min_length' => 'Le prénom doit faire plus de 2 caractères',
+            'max_length' => 'Le prénom doit faire moins de 100 caractères',
+        ],
+        'last_name' => [
+            'required' => 'Nom de famille requis',
+            'min_length' => 'Le nom de famille doit faire plus de 2 caractères',
+            'max_length' => 'Le nom de famille doit faire moins de 100 caractères',
+        ],
         'email' => [
             'required' => "Adresse email requise",
             'valid_email' => "Adresse email invalide",
+            'is_unique' => "Cette adresse est déjà utilisée",
         ],
         'password' => [
             'required'   => 'Un mot de passe est requis',
@@ -44,6 +61,18 @@ class UserModel extends Model
             'max_length' => 'Le mot de passe ne doit pas dépasser 255 caractères',
             'regex_match' => 'Le mot de passe doit contenir au moins une majuscule, minuscule, un nombre et un caractère spécial',
         ],
+        'mobile' => [
+            'regex_match' => 'Numéro de téléphone invalide',
+        ],
+        'birth_date' => [
+            'required' => 'Date de naissance requise',
+            'valid_date' => 'Date de naissance invalide (doit être format Y-m-d)',
+
+        ],
+        'gender' => [
+            'required' => 'Genre requis',
+            'in_list' => 'Genre invalide',
+        ]
     ];
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
