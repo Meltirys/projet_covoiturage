9f8dc23fd688e238007e217aac98139cacf62c63
Zadig <odium@riseup.net>
2026-05-07 09:59:17 +0200

    +login routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
+<<<<<<< HEAD
 // C'est ici que l'on défini les chemins des pages
 // Pour en définir une nouvelle il suffit d'écrire comme ceci :
 // $routes->méthodehttp(get ou post)('url que l'on souhaite afficher', 'nomducontrolleur::méthode)
@@ -20,3 +21,10 @@ $routes->get('trajet', 'ItineraryController::index');
 $routes->post('auth/connexion', 'AuthController::login');
 
 $routes->post('auth/inscription', 'AuthController::register');
+=======
+// Login
+$routes->get('login', [Auth::class, 'login']);
+$routes->post('login', [Auth::class, 'loginAttempt']);
+$routes->get('register', [Auth::class, 'register']);
+$routes->post('register', [Auth::class, 'registerAttempt']);
+>>>>>>> fcaae1c (+login routes)
