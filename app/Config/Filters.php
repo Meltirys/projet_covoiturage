ba7018ba1fca5a85ab9e91993cdf71a793547f5c
Meltirys <mat.bourbon@hotmail.fr>
2026-05-07 15:46:55 +0200

    add CI4 filters

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -34,6 +34,9 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'auth'          => \App\Filters\AuthCheck::class,
+        'authadmin'     => \App\Filters\AuthAdmin::class,
+        'authsuper'     => \App\Filters\AuthSuperAdmin::class,
     ];
 
     /**
