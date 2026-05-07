ef07a8a096cbb5d50241114be005cd8b6f62165e
Zadig <odium@riseup.net>
2026-05-07 16:28:29 +0200

    changed method name from index to home for clarity

--- app/Controllers/PagesController.php
+++ app/Controllers/PagesController.php
@@ -7,25 +7,9 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class PagesController extends BaseController
 {
-    public function index()
+    public function home()
     {
         helper('form');
         return view('HomeView');
-    }
-
-    public function itineraries()
-    {
-        helper('form');
-        return view('commons/header')
-            . view('itinerary/search/SearchView')
-            . view('commons/footer');
-    }
-
-    public function newItinerary()
-    {
-        helper('form');
-        return view('commons/header')
-            . view('itinerary/create/CreateView')
-            . view('commons/footer');
     }
 }
