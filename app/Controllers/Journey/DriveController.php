8bda22133d25ac1a6bd3b10aa4d7e33759f5edbb
Zadig <odium@riseup.net>
2026-05-13 14:13:34 +0200

    refactored save function

--- app/Controllers/Journey/DriveController.php
+++ app/Controllers/Journey/DriveController.php
@@ -5,11 +5,10 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Exceptions\PageNotFoundException;
-use App\Validators\RegistrationValidator;
 use App\Models\JourneyDriveModel;
 use App\Models\CarModel;
-use App\Models\CityModel;
-use App\Models\LocationModel;
+use App\Validators\JourneyDriveValidator;
+use PDOException;
 
 class ItineraryController extends BaseController
 {
@@ -69,100 +68,54 @@ class ItineraryController extends BaseController
      */
     public function save()
     {
-        // Etapes :
-        // 1. Vérifier voiture
-        // 2. Créer city/location
-        // 3. Créer trajet
-        // 4. Créer stages
-        // 5. Insérer options
-
         helper('form');
 
-        $journey = [
-            'number_of_place'      => intval($this->request->getPost('seats')),
-            'departure' => $this->request->getPost('start-time'),
-            'estimated_arrival' => $this->request->getPost('end-time'),
-            'id_car'        => intval($this->request->getPost('car')),
-        ];
+        /* Needed inputs
+         * id_car, start_city, start_city_postcode, end_city, end_city_postcode,
+         * start, start_lat, start_long,
+         * end, end_lat, end_long,
+         * number_of_place, departure, estimated_arrival
+         * stops[city_name, city_postcode, address, latitude, longitude]
+         * 
+         * options?
+         */
 
-        $startLocation = [
-            'start'      => $this->request->getPost('start'),
-            'start_lat' => 10,
-            'start_long' => 10,
-        ];
+        $data = $this->request->getPost();
 
-        $startCity = [
-            'start_city' => mb_convert_case(trim('Vannes'), MB_CASE_TITLE, "UTF-8"),
-            'start_city_postcode' => '56000',
-        ];
+        // Validation
+        $validator = new JourneyDriveValidator;
 
-        $endLocation = [
-            'end' => $this->request->getPost('end'),
-            'end_lat' => 10,
-            'end_long' => 10,
-        ];
-
-        $endCity = [
-            'end_city' => mb_convert_case(trim('Vannes'), MB_CASE_TITLE, "UTF-8"),
-            'end_city_postcode' => '56000',
-        ];
-
-        // Reste :
-        # 'options'    => $this->request->getPost('options')
-        # 'stops'       => $this->request->getPost('stop'),
-
-        // TODO : Validation step
-
-        // === Gets car
-        $carModel = model(CarModel::class);
-
-        $car = $carModel
-            ->where('id_car', $journey['car_id'])
-            ->where('id_user', session()->get('user_id'))
-            ->first();
-
-        if (! $car) {
+        if (! $validator->validate($data)) {
             return redirect()->back()
-                ->with('error', 'Voiture invalide');
+                ->with('errors', $validator->getErrors())
+                ->withInput();
         }
-        // ===
 
-        // === Gets city (insertion if didn't exist)
-        $cityModel = model(CityModel::class);
+        // Logic
+        try {
+            $journeyService = service('journeyService');
 
-        $startCityID = $cityModel->getOrCreate($startCity['start_city'], $startCity['start_city_postcode']);
-        $endCityID = $cityModel->getOrCreate($endCity['end_city'], $endCity['end_city_postcode']);
-        //
-
-        // === Gets location (insertion if didn't exist)
-        $locationModel = model(LocationModel::class);
-
-        $journey['start'] = $locationModel->getOrCreate($startLocation['start'], $startCityID, $startLocation['start_lat'], $startLocation['start_long']);
-        $journey['end'] = $locationModel->getOrCreate($endLocation['end'], $endCityID, $endLocation['end_lat'], $endLocation['end_long']);
-        // ===
-
-        // === Creates journey
-        $journeyDriveModel = model(JourneyDriveModel::class);
-
-
-        if (! $journeyDriveModel->save($journey)) {
-            $errors = $journeyDriveModel->errors();
+            // === Ajouter options quand possible !
+            $journeyId = $journeyService->createJourneyDrive(
+                $data,
+                session()->get('user_id')
+            );
 
             return redirect()->to('/')
-                ->with('errors', $errors)
-                ->withInput()
-                ->with('error', 'Votre trajet n\'a pas pu être créé');
+                ->with('status', 'Itinéraire créé avec succès');
+        } catch (\DomainException $e) {
+            // user error (e.g. chosen more seats than available in car)
+            return redirect()->back()
+                ->with('error', $e->getMessage())
+                ->withInput();
+        } catch (\Throwable $e) {
+            // system error
+            log_message('error', $e->getMessage());
+
+            return redirect()->back()
+                ->with('error', 'Une erreur s\'est produite')
+                ->withInput();
         }
-        // ===
-
-        // === Créer stages
-        // ===
-
-        // === Ajouter options
-        // ===
-
-        return redirect()->to('/')
-            ->with('status', 'Itinéraire créé avec succès');
     }
 
     /**
