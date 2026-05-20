f8d7258110de4c781e822b1739e4b266c9e28ebc
Zadig <odium@riseup.net>
2026-05-20 16:19:15 +0200

    moved to authentification gated space

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -45,6 +45,26 @@ $routes->group('', ['filter' => 'auth'], function ($routes) {
     $routes->post('user/update', [UserController::class, 'update']);
     $routes->post('user/updatePassword', [UserController::class, 'updatePassword']);
     $routes->post('user/delete', [UserController::class, 'delete']);
+
+    // Itinéraire (conducteur)
+    $routes->get('drive/search', 'Journey\DriveController::search');
+    $routes->get('drive/show', 'Journey\DriveController::show');
+    $routes->get('drive/create', 'Journey\DriveController::create');
+    $routes->post('drive/save', 'Journey\DriveController::save');
+
+    // Itinéraire (requête)
+    $routes->get('request/search', 'Journey\RequestController::search');
+    $routes->get('request/show', 'Journey\RequestController::show');
+    $routes->get('request/create', 'Journey\RequestController::create');
+    $routes->post('request/save', 'Journey\RequestController::save');
+
+    // Réservation
+    $routes->get('reservation/(:num)',           'Journey\BookingController::show/$1');
+    $routes->post('reservation',                 'Journey\BookingController::save');
+    $routes->get('mes-reservations',             'Journey\BookingController::index');
+    $routes->post('reservation/annuler/(:num)',  'Journey\BookingController::cancel/$1');
+    $routes->post('reservation/accepter/(:num)', 'Journey\BookingController::accept/$1');
+    $routes->post('reservation/refuser/(:num)',  'Journey\BookingController::refuse/$1');
 });
 
 // Itinéraire (conducteur)
