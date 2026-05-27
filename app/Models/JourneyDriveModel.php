f3953969864750adc0b4d08a64e0eff8601cdd2f
Zadig <odium@riseup.net>
2026-05-27 10:32:36 +0200

    added driver column

--- app/Models/JourneyDriveModel.php
+++ app/Models/JourneyDriveModel.php
@@ -12,7 +12,7 @@ class JourneyDriveModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['number_of_place', 'departure', 'estimated_arrival', 'id_track', 'start', 'end', 'id_car'];
+    protected $allowedFields    = ['number_of_place', 'departure', 'estimated_arrival', 'id_track', 'start', 'end', 'id_car', 'driver'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
