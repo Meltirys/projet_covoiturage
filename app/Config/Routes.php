3d5aa7c848b7dccf13ca4787492edadec204de3d
Zadig <odium@riseup.net>
2026-06-09 11:10:23 +0200

    created journeydrive edit routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -66,6 +66,10 @@ $routes->group('', ['filter' => 'auth'], function ($routes) {
     $routes->post('request/save', 'Journey\RequestController::save'); // requête
     $routes->get('request/list', 'Journey\RequestController::index');
 
+    // Modification de trajet conducteur
+    $routes->get('trajet/modification/(:num)', 'PagesController::editJourneyDrive/$1');
+    $routes->post('drive/edit', 'Journey\DriveController::update');
+
     // Modification de demande de trajet
     $routes->get('request/edit/(:num)', 'Journey\RequestController::edit/$1');
     $routes->post('request/update/(:num)', 'Journey\RequestController::update/$1');
