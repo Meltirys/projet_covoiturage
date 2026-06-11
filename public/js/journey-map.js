aa56edd0f2275449a20158b5d0cd5116221d489a
Meltirys <mat.bourbon@hotmail.fr>
2026-06-11 16:04:49 +0200

    adjust logic variable name

--- public/js/journey-map.js
+++ public/js/journey-map.js
@@ -1,8 +1,8 @@
-const el = document.getElementById('map');
-if (el && el.dataset.geojson) {
+const mapContainer = document.getElementById('map');
+if (mapContainer && mapContainer.dataset.geojson) {
     const map = L.map('map').setView([46.5, 2.5], 6);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-    const layer = L.geoJSON(JSON.parse(el.dataset.geojson)).addTo(map);
+    const layer = L.geoJSON(JSON.parse(mapContainer.dataset.geojson)).addTo(map);
     map.fitBounds(layer.getBounds());
 }
