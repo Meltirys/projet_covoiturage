07f928408f2375eeeaf6b7b894b0960909971d6c
Zadig <odium@riseup.net>
2026-06-15 10:56:54 +0200

    new validator for JourneyRequest search (WIP)

---
+++ app/Validators/JourneyRequest/SearchJourneyRequestValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Validators\JourneyRequest;
+
+use App\Validation\CustomRules;
+use App\Validators\BaseValidator;
+
+class SearchJourneyRequestValidator extends BaseValidator
+{
+    /**
+     * List the rules that needs to be followed in order to be valid
+     * @return array Array containing the rules
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+}
