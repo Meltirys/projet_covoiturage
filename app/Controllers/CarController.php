52128bbb285dd8ec5157f9325e1f90ab4a6079bd
Zadig <odium@riseup.net>
2026-06-15 15:50:33 +0200

    replaced 404 with pagenotfoundexceptions and fixed a typo

--- app/Controllers/CarController.php
+++ app/Controllers/CarController.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\CarModel;
 use App\Validators\CarValidator;
-
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class CarController extends BaseController
 {
@@ -73,15 +73,15 @@ class CarController extends BaseController
 
         //If the user is not the owner, we redirect him
         if (!$isOwner) {
-            return redirect('404');
+            throw PageNotFoundException::forPageNotFound();
         }
 
         //Chcking if the car is used on future journey
         $journeyDriveModel = model('JourneyDriveModel');
 
-        if($journeyDriveModel->futureJourneyByCar($idCar)){
+        if ($journeyDriveModel->futureJourneyByCar($idCar)) {
             return redirect()->to('/myprofil')
-                ->with('car_error', 'Impossible de supprimer une voiture si elle est utilisé dans un futur trajet. Veuillez supprimer le/les trajets avant de supprimer la voiture');
+                ->with('car_error', 'Impossible de supprimer une voiture si elle est utilisé dans un futur trajet. Veuillez modifier ou supprimer les trajets avant de supprimer la voiture');
         }
 
         if (! $carModel->delete($idCar)) {
@@ -105,7 +105,7 @@ class CarController extends BaseController
 
         //If the user is not the owner, we redirect him
         if (!$isOwner) {
-            return redirect('404');
+            throw PageNotFoundException::forPageNotFound();
         }
 
         $car = [
@@ -117,7 +117,7 @@ class CarController extends BaseController
         ];
 
         //Calling the specific validator
-        $validator = new CarValidator();
+        $validator = new CarValidator;
 
         //If an error is detected, we redirect the errors to the profil controller
         if (!$validator->validate($car)) {
@@ -125,7 +125,7 @@ class CarController extends BaseController
                 ->with('error_in_modify_car_form', true) //This variable is meant to tell the view that it needs to show the form to modify a car on load
                 ->withInput()
                 ->with('errors', $validator->getErrors()) //We transfer the errors so the view can display it
-                ->with('idCar' , $idCar); //Transfering the id of the car so the form can point to the correct car
+                ->with('idCar', $idCar); //Transfering the id of the car so the form can point to the correct car
         }
 
         //Adding the user id for the database request
@@ -137,7 +137,7 @@ class CarController extends BaseController
             return redirect()->to('/myprofil')
                 ->with('errors', $errors)
                 ->withInput()
-                ->with('car_error', 'Une erreur est survenue lors de la mdoficiation du véhicule');
+                ->with('car_error', 'Une erreur est survenue lors de la modification du véhicule');
         }
 
         return redirect()->to('/myprofil')
