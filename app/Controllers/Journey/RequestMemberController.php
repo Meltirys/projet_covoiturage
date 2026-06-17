94827ed48ac3c09bff219239c62a4922e75928e5
Zadig <odium@riseup.net>
2026-06-17 16:28:23 +0200

    new controller for RequestMembers

---
+++ app/Controllers/Journey/RequestMemberController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class RequestController extends BaseController
+{
+    public function save(
+        #
+    );
+
+    public function read(
+        #
+    );
+
+    public function update(
+        #
+    );
+
+    public function delete(
+        #
+    );
+}
