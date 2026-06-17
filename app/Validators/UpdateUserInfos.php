f507c774a5f51b60a33f6274ed9ab76bbfedc2a5
Johan LE GUENNEC <johan.leguennec20@gmail.com>
2026-06-17 12:36:15 +0200

    repairing the update profil

--- app/Validators/UpdateUserInfos.php
+++ app/Validators/UpdateUserInfos.php
@@ -104,21 +104,6 @@ class UpdateUserInfos extends BaseValidator
                     'regex_match' => 'Le format du code postal n\'est pas valide'
                 ]
             ],
-
-            'avatar' => [
-                'rules' => [
-                    'is_image[avatar]',      // C'est bien une image
-                    'mime_in[avatar,image/jpg,image/jpeg,image/png,image/webp]', // Types autorisés
-                    'max_size[avatar,2048]', // Taille max en Ko (ici 2Mo)
-                    'max_dims[avatar,1920,1080]', // Dimensions max en pixels
-                ],
-                'errors' => [
-                    'is_image'  => 'Le fichier doit être une image',
-                    'mime_in'   => 'Format accepté : jpg, jpeg, png, webp',
-                    'max_size'  => 'L\'image ne doit pas dépasser 2Mo',
-                    'max_dims'  => 'L\'image ne doit pas dépasser 1920x1080 pixels',
-                ]
-            ],
         ];
     }
 }
