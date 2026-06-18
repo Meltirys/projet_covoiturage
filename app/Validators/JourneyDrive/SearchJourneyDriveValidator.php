94e48493f0e8211d052ea636da7c14f4b99b3049
Zadig <odium@riseup.net>
2026-06-18 15:58:12 +0200

    added date rule that makes sure input date is not in the past

--- app/Validators/JourneyDrive/SearchJourneyDriveValidator.php
+++ app/Validators/JourneyDrive/SearchJourneyDriveValidator.php
@@ -46,7 +46,7 @@ class SearchJourneyDriveValidator extends BaseValidator
             ]
         ];
         $dateRules = [
-            'rules' => 'required|valid_date',
+            'rules' => 'required|valid_date|equal_or_after_now',
             'errors' => [
                 'required' => 'La date est obligatoire.',
                 'valid_date' => 'La date est invalide.',
@@ -125,9 +125,8 @@ class SearchJourneyDriveValidator extends BaseValidator
             // ===== OTHER
 
             'free-seats'      => [
-                'rules' => 'required|greater_than_equal_to[1]|less_than_equal_to[8]',
+                'rules' => 'permit_empty|greater_than_equal_to[1]|less_than_equal_to[8]',
                 'errors' => [
-                    'required' => 'Le nombre de places disponsibles est obligatoire',
                     'greater_than_equal_to' => 'Le nombre de places choisi ne doit pas être inférieur à 1',
                     'less_than_equal_to' => 'Le nombre de places choisi est trop grand',
                 ]
