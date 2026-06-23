958ddf5bbfc39dec77da85093a90b76e62419388
Johan LE GUENNEC <johan.leguennec20@gmail.com>
2026-06-23 11:53:45 +0200

    adding securities to the booking, it now checks if the journey has been deleted before taking action

--- app/Controllers/Journey/BookingController.php
+++ app/Controllers/Journey/BookingController.php
@@ -11,6 +11,8 @@ use App\Validators\BookingValidator;
 use App\Services\MailService;
 use PhpParser\Node\Expr\AssignOp\Mod;
 
+use function PHPUnit\Framework\isNull;
+
 class BookingController extends BaseController
 {
 
@@ -30,8 +32,8 @@ class BookingController extends BaseController
         $bookingModel = new BookingModel();
         $journeyID = $this->request->getPost('id_journey_drive');
         $journey   = $journeyModel->find($journeyID);
-        // If route doesn't exist
-        if (!$journey) {
+        // If route doesn't exist or has been deleted
+        if (!$journey || !isNull($journey['deletion_date'])) {
             return redirect()->back()
                 ->with('error', 'Trajet introuvable');
         }
@@ -139,9 +141,9 @@ class BookingController extends BaseController
         $journeyModel = new JourneyDriveModel();
         $journey      = $journeyModel->find($booking['id_journey_drive']);
 
-        if ($journey['departure'] <= date('Y-m-d H:i:s')) {
+        if ($journey['departure'] <= date('Y-m-d H:i:s') || !isNull($journey['deletion_date'])) {
             return redirect()->to('myprofil')
-                ->with('error', 'Impossible d\'annuler un trajet passé');
+                ->with('error', 'Impossible d\'annuler un trajet passé ou supprimé');
         }
 
         $infos = $this->gatherMailInfos($id_booking); //Gather infos before deletion
@@ -191,13 +193,12 @@ class BookingController extends BaseController
                 ->with('error', 'Action non autorisée');
         }
 
-        if ($journey['departure'] <= date('Y-m-d H:i:s')) {
+        if ($journey['departure'] <= date('Y-m-d H:i:s') || !isNull($journey['deletion_date'])) {
             return redirect()->to('myprofil')
-                ->with('error', 'Impossible de valider une réservation d\'un trajet passé');
+                ->with('error', 'Impossible de valider une réservation d\'un trajet passé ou supprimé');
         }
 
         // Cheking if seats are still available
-
         $seatPicked = (int) $bookingModel
             ->selectSum('seat_taken')
             ->where('id_journey_drive', $journey['id_journey_drive'])
@@ -264,9 +265,9 @@ class BookingController extends BaseController
                 ->with('error', 'Action non autorisée');
         }
 
-        if ($journey['departure'] <= date('Y-m-d H:i:s')) {
+        if ($journey['departure'] <= date('Y-m-d H:i:s' || !isNull($journey['deletion_date']))) {
             return redirect()->to('myprofil')
-                ->with('error', 'Refus impossible sur un trajet déjà passé');
+                ->with('error', 'Refus impossible sur un trajet déjà passé ou supprimé');
         }
 
 
@@ -355,9 +356,9 @@ class BookingController extends BaseController
                 ->with('error', 'Trajet introuvable ou action non autorisée');
         }
         // If the journey is no longer active
-        if ($journey['departure'] <= date('Y-m-d H:i:s')) {
+        if ($journey['departure'] <= date('Y-m-d H:i:s') || !isNull($journey['deletion_date'])) {
             return redirect()->to('myprofil')
-                ->with('error', 'Impossible d\'annuler un trajet passé');
+                ->with('error', 'Impossible d\'annuler un trajet passé ou déjà annulé');
         }
 
         //Retrivieving the ids of the users on the journey
