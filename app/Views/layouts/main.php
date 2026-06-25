24de82e3ab3ca2181d64d73dcb27ad6a494e9939
ameliealb <bourdin.alb@gmail.com>
2026-06-25 09:53:08 +0200

    delete style in main

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -180,20 +180,6 @@
         </div>
     </footer>
 
-    <style>
-        @keyframes blink {
-
-            0%,
-            100% {
-                opacity: 1;
-            }
-
-            50% {
-                opacity: 0.4;
-            }
-        }
-    </style>
-
 </body>
 
 </html>
